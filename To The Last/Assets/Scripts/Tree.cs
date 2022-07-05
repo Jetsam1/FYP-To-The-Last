256f662ed916093e08948b012349a31f5866cfd1
Jetsam <Akash9105@hotmail.com>
2022-07-05 16:42:21 +0100

    WE HAVE PATROLLING

--- To The Last/Assets/Scripts/Tree.cs
+++ To The Last/Assets/Scripts/Tree.cs
@@ -11,13 +11,14 @@ namespace BehaviourTree
         // Start is called before the first frame update
         protected void Start()
         {
-
+            root = SetupTree();
         }
 
         // Update is called once per frame
         private void Update()
         {
-
+            if (root != null)
+                root.Eval();
         }
 
         protected abstract Node SetupTree();
